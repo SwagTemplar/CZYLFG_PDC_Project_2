a9fe0b3228ee618a8e09bba59d560a976a0b611d
Conzeyy <connorstwrt@gmail.com>
2022-10-19 10:35:25 +1300

    Adding Gui content

---
+++ CZYLFG_Project_2/src/NewPage.java
@@ -0,0 +1,16 @@
+
+
+/*
+ * @author "Connor Stewart 17982915"
+ */
+class NewPage extends JFrameLogin {
+
+    NewPage() {
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Welcome");
+        setSize(700, 500);
+        NewPage nPage = new NewPage();
+        nPage.setLocationRelativeTo(null);
+        nPage.setVisible(true);
+    }
+}
