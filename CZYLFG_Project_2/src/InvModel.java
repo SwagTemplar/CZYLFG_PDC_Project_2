4e74833a22a47aa4297bf7c730efd3ef65f0f06d
Conzeyy <connorstwrt@gmail.com>
2022-10-25 23:50:19 +1300

    0.0.1 Beginnings of InvController

--- CZYLFG_Project_2/src/InvModel.java
+++ CZYLFG_Project_2/src/InvModel.java
@@ -3,7 +3,15 @@
 /*
  * @author Connor Stewart 17982915
  */
-
 public class InvModel {
+
+    public String username;
+//    public Database db;
+//    public Data data;
+
+    void checkLogin(String username, String password) {
+        this.username = username;
+//      this.data = this.db.checkName(username, password);
+    }
 
 }
