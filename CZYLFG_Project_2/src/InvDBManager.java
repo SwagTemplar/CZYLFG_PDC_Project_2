b3a92d417a74cb85aa4d4d3e28c74835e0feead7
SwagTemplar <kisoonpark.red@gmail.com>
2022-10-26 22:54:46 +1300

    0.2.1 Additional Methods
    Started working with the model

--- CZYLFG_Project_2/src/InvDBManager.java
+++ CZYLFG_Project_2/src/InvDBManager.java
@@ -21,9 +21,15 @@ public class InvDBManager {
         createTables();
     }
 
+    //Create the tables in the embedded DB
     private void createTables() {
         if (!checkTableExist("LOGINDETAILS")) {
             databaseUpdate("CREATE TABLE LOGINDETAILS(USERID INT, USERNAME VARCHAR(30), PASSWORD VARCHAR(30))");
+            databaseQuery("INSERT INTO LOGINDETAILS VALUES(1, kisoon, leaf)");
+            databaseQuery("INSERT INTO LOGINDETAILS VALUES(2, kisoon, general)");
+            databaseQuery("INSERT INTO LOGINDETAILS VALUES(3, connor, conzey)");
+            databaseQuery("INSERT INTO LOGINDETAILS VALUES(4, connor, stewart)");
+                
         }
         if (!checkTableExist("INVENTORYSTOCK")) {
             databaseUpdate("CREATE TABLE INVENTORYSTOCK(FRUITID INT, FRUITNAME VARCHAR(40), FRUITQUANTITY INT)");
@@ -33,6 +39,7 @@ public class InvDBManager {
         }
     }
 
+    //Check if the tables exist
     private boolean checkTableExist(String name) {
         boolean exists = false;
 
@@ -52,6 +59,7 @@ public class InvDBManager {
         return exists;
     }
 
+    //Method that helps in creating the tables
     private void databaseUpdate(String statement) {
         try {
             sql = conn.createStatement();
@@ -63,6 +71,7 @@ public class InvDBManager {
         }
     }
 
+    //Method that queries the database
     private ResultSet databaseQuery(String statement) {
         ResultSet queryResult = null;
         try {
@@ -72,5 +81,31 @@ public class InvDBManager {
             System.out.println("Invalid SQL command");
         }
         return queryResult;
+    }
+    
+    //Checks if the login details of the user exist.
+    public User checkLogin(String username, String password)
+    {
+        User user = new User();
+        
+        try{
+            ResultSet loginSearch = databaseQuery("SELECT USERNAME, PASSWORD FROM LOGINDETAILS WHERE USERNAME = '"+ username +"'");
+            
+            if(loginSearch.next())
+            {
+                String pw = loginSearch.getString("PASSWORD");
+                
+                if(password.compareTo(pw) == 0)
+                {
+                    user.loginFlag = true;
+                    user.userID = loginSearch.getInt("USERID");
+                }
+            }
+        }catch(SQLException ex)
+        {
+            System.out.println("No such user exists");
+        }
+        
+        return user;
     }
 }
