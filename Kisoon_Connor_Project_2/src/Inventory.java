9ae9f12cd0f142f49dfdb3a51d12da84034dfb1b
SwagTemplar <kisoonpark.red@gmail.com>
2022-10-30 23:30:58 +1300

    0.0.1 Inventory
    Created inventory class

---
+++ Kisoon_Connor_Project_2/src/Inventory.java
@@ -0,0 +1,55 @@
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kisoo
+ */
+public class Inventory {
+
+    ArrayList<Item> itemArr;
+
+    public Inventory() {
+        itemArr = new ArrayList<>();
+    }
+
+    public void add(Item item) {
+        itemArr.add(item);
+    }
+
+    public String toString() {
+        String str = "";
+        for (Item i : itemArr) {
+            str += "Item ID: " + i.getItemID() + " Item Name: " + i.getItemName() + " In Stock: " + i.getQuantityInStock() + "\n";
+        }
+
+        return str;
+    }
+
+    public boolean stockCheck(int itemID, int quantity) {
+        boolean inStock = false;
+
+        for (Item i : itemArr) {
+            if (itemID == i.getItemID()) {
+                if (quantity >= 1 && i.getQuantityInStock() >= quantity) {
+                    inStock = true;
+                } else {
+                    System.out.println("Invalid Quantity");
+                }
+            }
+        }
+        return inStock;
+    }
+
+    public void increaseInventory(int itemID, int quantity) {
+        for (Item i : itemArr) {
+            if (itemID == i.getItemID()) {
+                if (quantity >= 1 && quantity < 99999) {
+                    i.increaseStock(quantity);
+                } else {
+                    System.out.println("Invalid Quantity");
+                }
+            }
+        }
+    }
+}
