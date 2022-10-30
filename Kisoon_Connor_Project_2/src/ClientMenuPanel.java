f140b44c0da8d86e8d68ea19199b8bcc8477e0b3
Conzeyy <connorstwrt@gmail.com>
2022-10-31 04:24:10 +1300

    0.0.2 Comments

--- Kisoon_Connor_Project_2/src/ClientMenuPanel.java
+++ Kisoon_Connor_Project_2/src/ClientMenuPanel.java
@@ -10,28 +10,28 @@ import javax.swing.JPanel;
  * @author Kisoon Park 18008173 & Connor Stewart 17982915
  */
 class ClientMenuPanel extends JPanel {
-
+    // Variables
     private InvView invview;
-
+    // Components that will be used in the panel
     private JButton viewCurrentInventory;
     private JButton createOrder;
     private JButton viewCurrentOrder;
     private JButton logOut;
     private GridBagConstraints gBC;
     private JLabel message;
-
+    // Constructor setting size to invview
     public ClientMenuPanel(InvView invview) {
         this.invview = invview;
         this.setSize(invview.getWidth(), invview.getHeight());
         components();
     }
-
+    // adding all the components into the panel
     private void components() {
 
         gBC = new GridBagConstraints();
-
+        // sets bounds for components using GridBagConstraints and insets
         gBC.insets = new Insets(10, 10, 10, 10);
-
+        // adds button
         viewCurrentInventory = new JButton();
         viewCurrentInventory.setText("View current inventory");
         viewCurrentInventory.setVisible(true);
@@ -39,7 +39,7 @@ class ClientMenuPanel extends JPanel {
         gBC.gridx = 0;
         gBC.gridy = 1;
         this.add(viewCurrentInventory, gBC);
-
+        // adds button
         createOrder = new JButton();
         createOrder.setText("Create order");
         createOrder.setVisible(true);
@@ -47,7 +47,7 @@ class ClientMenuPanel extends JPanel {
         gBC.gridx = 0;
         gBC.gridy = 2;
         this.add(createOrder, gBC);
-
+        // adds button
         viewCurrentOrder = new JButton();
         viewCurrentOrder.setText("view current orders");
         viewCurrentOrder.setVisible(true);
@@ -55,7 +55,7 @@ class ClientMenuPanel extends JPanel {
         gBC.gridx = 0;
         gBC.gridy = 3;
         this.add(viewCurrentOrder, gBC);
-
+        // adds button
         logOut = new JButton();
         logOut.setText("Logout");
         logOut.setVisible(true);
@@ -63,7 +63,7 @@ class ClientMenuPanel extends JPanel {
         gBC.gridx = 0;
         gBC.gridy = 4;
         this.add(logOut, gBC);
-
+        // adds label to let user know they logged in on client side
         message = new JLabel();
         this.add(this.message, BorderLayout.SOUTH);
         message.setText("Client login successful");
