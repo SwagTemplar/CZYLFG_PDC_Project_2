1e5b3ef22bfc55bf505ed16a6ec410d65ee9744e
SwagTemplar <kisoonpark.red@gmail.com>
2022-10-30 19:13:48 +1300

    0.2.0 User added methods

--- Connor_Kisoon_Project_2/src/connor_kisoon_project_2/User.java
+++ Connor_Kisoon_Project_2/src/connor_kisoon_project_2/User.java
@@ -5,6 +5,24 @@ package connor_kisoon_project_2;
  * @author kisoo
  */
 public class User {
+    
+    //Checks login
     boolean loginFlag = false;
+    
+    //User ID
     int userID;
+    
+    public void setLoginFlag(boolean bool)
+    {
+        this.loginFlag = bool;
+    }
+    
+    public void setUserId(int id){
+        this.userID = id;
+    }
+    
+    public void userLogout()
+    {
+        this.loginFlag = false;
+    }
 }
