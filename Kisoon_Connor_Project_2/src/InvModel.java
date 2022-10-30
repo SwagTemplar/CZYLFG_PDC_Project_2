45d3f238310e6c27b66839b69b7e1c0339d3ff16
Conzeyy <connorstwrt@gmail.com>
2022-10-31 04:25:11 +1300

    0.1.2 Comments

--- Kisoon_Connor_Project_2/src/InvModel.java
+++ Kisoon_Connor_Project_2/src/InvModel.java
@@ -7,27 +7,32 @@ import java.util.Observable;
  * @author Kisoon Park 18008173 & Connor Stewart 17982915
  */
 public class InvModel extends Observable{
-
+    // Creation of Variables
     private InvDBManager db;
     public User user;
     private ClientUser cUser;
     private AdminUser adUser;
-
+    // Constructor
     public InvModel(){
         db = new InvDBManager();
     }
     
+    /**
+     * @return the ClientUser
+     */
     public ClientUser getClient()
     {
         return this.cUser;
     }
-    
+    /**
+     * @return the AdminUser
+     */
     public AdminUser getAdmin()    
     {
         return this.adUser;
     }
     
-    
+    // Checks username and password to database
     public void validateUser(String username, String password) {
         
         user = db.checkLogin(username, password);
@@ -43,12 +48,12 @@ public class InvModel extends Observable{
                 cUser.userID = user.userID;    
             }
     }
-    
+    // retrieves client orders from database
     public OrderList clientOrders()
     {
         return db.retrieveOrders(cUser.userID);
     }
-    
+    // updates the observer when called
     public void updateObs(){        
         this.setChanged();
         this.notifyObservers(this);
