cc427fa8897c402b45dd84e0ecfbfea5e8d5f050
Conzeyy <connorstwrt@gmail.com>
2022-10-31 04:25:36 +1300

    0.0.2 Comments

--- Kisoon_Connor_Project_2/src/LoginPanel.java
+++ Kisoon_Connor_Project_2/src/LoginPanel.java
@@ -11,9 +11,9 @@ import javax.swing.JTextField;
  * @author Kisoon Park 18008173 & Connor Stewart 17982915
  */
 public class LoginPanel extends JPanel {
-
+    // Variable
     private InvView invview;
-
+    // Components that will be used in the panel
     private JButton logIn;
     private JTextField usernameInput;
     private JTextField passwordInput;
@@ -23,44 +23,48 @@ public class LoginPanel extends JPanel {
     private JLabel userLoginDetails;
     private JLabel passLoginDetails;
     private GridBagConstraints gBC;
-
+// Constructor setting size to invview
     public LoginPanel(InvView invview) {
         this.invview = invview;
         this.setSize(invview.getWidth(), invview.getHeight());
         components();
     }
-
+// adding all the components into the panel
     private void components() {
 
         gBC = new GridBagConstraints();
-        // Login Button
-
+        // sets bounds for components using GridBagConstraints and insets
         gBC.insets = new Insets(10, 10, 10, 10);
-
+        
+        // adds label to inform user what textfield they are typing in
         username = new JLabel("Username");
         gBC.gridx = 0;
         gBC.gridy = 0;
         this.add(username, gBC);
         username.setVisible(true);
 
+        // adds textfeild for user to type username
         usernameInput = new JTextField(30);
         gBC.gridx = 1;
         gBC.gridy = 0;
         this.add(usernameInput, gBC);
         usernameInput.setVisible(true);
 
+        // adds label to inform user what textfield they are typing in
         password = new JLabel("Password");
         gBC.gridx = 2;
         gBC.gridy = 0;
         this.add(password, gBC);
         password.setVisible(true);
 
+        // adds textfeild for user to type password
         passwordInput = new JTextField(30);
         gBC.gridx = 3;
         gBC.gridy = 0;
         this.add(passwordInput, gBC);
         passwordInput.setVisible(true);
 
+        // adds button to login with
         logIn = new JButton();
         gBC.anchor = GridBagConstraints.CENTER;
         gBC.gridx = 1;
@@ -69,14 +73,17 @@ public class LoginPanel extends JPanel {
         getLogIn().setText("Login");
         getLogIn().setVisible(true);
 
+        // adds label to prompt user to input login information
         message = new JLabel();
         message.setText("Please Login to continue");
         this.add(this.message, BorderLayout.SOUTH);
         
+        // adds label so marker can login
         userLoginDetails = new JLabel();
         userLoginDetails.setText("Client login: Username = bob");
         this.add(this.userLoginDetails, BorderLayout.SOUTH);
         
+        // adds label so marker can login
         passLoginDetails = new JLabel();
         passLoginDetails.setText("Client login: Password = 123");
         this.add(this.passLoginDetails, BorderLayout.SOUTH);
