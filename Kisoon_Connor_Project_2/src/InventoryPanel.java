24b7171bd17f85c0e4b7cfc15c1b5a48be92353a
Conzeyy <connorstwrt@gmail.com>
2022-10-31 04:24:40 +1300

    0.0.2 Comments

--- Kisoon_Connor_Project_2/src/InventoryPanel.java
+++ Kisoon_Connor_Project_2/src/InventoryPanel.java
@@ -13,35 +13,39 @@ import javax.swing.ScrollPaneConstants;
  * @author Kisoon Park 18008173 & Connor Stewart 17982915
  */
 class InventoryPanel extends JPanel {
-
+// Variable
     private InvView invview;
-
+// Components that will be used in the panel
     private JLabel message;
     private JTextArea fileView;
     private JButton backButton;
     private GridBagConstraints gBC;
     private JScrollPane scrollPane;
-
+// Constructor setting size to invview
     public InventoryPanel(InvView invview) {
         this.invview = invview;
         this.setSize(invview.getWidth(), invview.getHeight());
         components();
     }
-
+// adding all the components into the panel
     private void components() {
+        
         gBC = new GridBagConstraints();
-
+        // sets bounds for components using GridBagConstraints and insets
         gBC.insets = new Insets(10, 10, 10, 10);
-
+        
+        // adds button to go back
         backButton = new JButton();
         backButton.setText("Back");
         gBC.anchor = GridBagConstraints.NORTH;
         backButton.setVisible(true);
         
+        // adds label to notify user what they are viewing
         message = new JLabel();
         this.add(this.message, BorderLayout.SOUTH);
         message.setText("Viewing Inventory");
         
+        // adds scrollpane and textarea to display database content of inventory
         this.add(backButton, gBC);
         this.add(scrollPane = new JScrollPane(new JTextArea(30,20)), BorderLayout.PAGE_START);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
