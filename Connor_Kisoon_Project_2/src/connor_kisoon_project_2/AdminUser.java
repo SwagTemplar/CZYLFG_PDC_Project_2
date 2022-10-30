f4fc66e3b23d5ff9f95a9c932ce74594f3a5545c
SwagTemplar <kisoonpark.red@gmail.com>
2022-10-30 19:10:07 +1300

    0.0.1 Initial AdminUser

---
+++ Connor_Kisoon_Project_2/src/connor_kisoon_project_2/AdminUser.java
@@ -0,0 +1,13 @@
+package connor_kisoon_project_2;
+
+/**
+ *  The users with the USERID of 1 or 2 will be admins
+ * 
+ * 
+ * @author kisoo
+ */
+public class AdminUser extends User{
+    
+    
+    
+}
