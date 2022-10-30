8ce802fc25660341923484ebd7f16ad73f044343
Conzeyy <connorstwrt@gmail.com>
2022-10-31 03:54:24 +1300

    0.2.3 InvView Clean up

--- Kisoon_Connor_Project_2/src/InvView.java
+++ Kisoon_Connor_Project_2/src/InvView.java
@@ -1,114 +1,60 @@
 
-
-import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.Toolkit;
-import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
 /*
  * @author Connor Stewart 17982915
  */
 public class InvView extends JFrame implements Observer {
 
-    private JPanel viewerPanel = new JPanel(new GridBagLayout());
-    private JPanel loginPanel = new JPanel(new GridBagLayout());
-    private JButton vCuInv = new JButton("View current inventory");
-    private JButton crOrd = new JButton("Create new order");
-    private JButton vCuOrd = new JButton("View current orders");
-    private JButton logOut = new JButton("Logout");
-    private JTextArea fileView = new JTextArea();
-    private JButton logIn = new JButton("Login");
-    public JTextField usernameInput = new JTextField(20);
-    public JTextField passwordInput = new JTextField(20);
-    private JLabel username = new JLabel("Username: ");
-    private JLabel password = new JLabel("Password: ");
-    public JLabel message = new JLabel("Please Login to continue", JLabel.CENTER);
-    public GridBagConstraints gBC = new GridBagConstraints();
+    private LoginPanel loginPanel;
+    private ClientMenuPanel clientMenuPanel;
+    private InventoryPanel inventoryPanel;
+    private CreateOrderPanel createOrderPanel;
 
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
     public InvView() {
         int userScreenWidth = screenSize.width / 2;
         int userScreenHeight = screenSize.height / 2;
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(userScreenWidth, userScreenHeight);
         this.setLocationRelativeTo(null);
+        components();
+        visiblePanels();
+    }
 
-        gBC.insets = new Insets(10, 10, 10, 10);
-
-        gBC.anchor = GridBagConstraints.CENTER;
-        gBC.gridx = 0;
-        gBC.gridy = 0;
-        this.loginPanel.add(username, gBC);
-
-        gBC.anchor = GridBagConstraints.CENTER;
-        gBC.gridx = 1;
-        gBC.gridy = 0;
-        this.loginPanel.add(usernameInput, gBC);
-
-        gBC.anchor = GridBagConstraints.CENTER;
-        gBC.gridx = 2;
-        gBC.gridy = 0;
-        this.loginPanel.add(password, gBC);
-
-        gBC.anchor = GridBagConstraints.CENTER;
-        gBC.gridx = 3;
-        gBC.gridy = 0;
-        this.loginPanel.add(passwordInput, gBC);
-
-        gBC.anchor = GridBagConstraints.CENTER;
-        gBC.gridx = 4;
-        gBC.gridy = 0;
-        this.loginPanel.add(logIn, gBC);
-
-        this.add(this.message, BorderLayout.SOUTH);
-
-        this.add(loginPanel);
+    private void components() {
+        this.loginPanel = new LoginPanel(this);
+        this.clientMenuPanel = new ClientMenuPanel(this);
+        this.inventoryPanel = new InventoryPanel(this);
+        this.createOrderPanel = new CreateOrderPanel(this);
+        this.add(getLoginPanel());
+        this.add(getClientMenuPanel());
+        this.add(getInventoryPanel());
+        this.add(getCreateOrderPanel());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
 
-    public void Viewer() {
-        gBC.insets = new Insets(10, 10, 10, 10);
-        gBC.gridx = 0;
-        gBC.gridy = 1;
-        this.viewerPanel.add(vCuInv, gBC);
-        gBC.gridx = 0;
-        gBC.gridy = 2;
-        this.viewerPanel.add(vCuOrd, gBC);
-        gBC.gridx = 0;
-        gBC.gridy = 3;
-        this.viewerPanel.add(crOrd, gBC);
-        gBC.gridx = 0;
-        gBC.gridy = 4;
-        this.viewerPanel.add(logOut, gBC);
-
-        this.getContentPane().removeAll();
-        this.add(this.message, BorderLayout.SOUTH);
-        this.add(new JScrollPane(fileView));
-        this.add(viewerPanel, BorderLayout.EAST);
-        viewerPanel.setVisible(true);
-        this.add(viewerPanel);
-        this.revalidate();
-        this.repaint();
+    private void visiblePanels() {
+        this.getLoginPanel().setVisible(true);
+        this.getClientMenuPanel().setVisible(false);
+        this.getInventoryPanel().setVisible(false);
+        this.getCreateOrderPanel().setVisible(false);
     }
 
-    void addActionListener(ActionListener listener) {
-        this.logIn.addActionListener(listener);
-        this.vCuInv.addActionListener(listener);
-        this.crOrd.addActionListener(listener);
-        this.vCuOrd.addActionListener(listener);
+    public void createControllers(InvController controller) {
+        getLoginPanel().getLogIn().addActionListener(controller);
+        getClientMenuPanel().getViewCurrentInventory().addActionListener(controller);
+        getClientMenuPanel().getViewCurrentOrder().addActionListener(controller);
+        getClientMenuPanel().getCreateOrder().addActionListener(controller);
+        getClientMenuPanel().getLogOut().addActionListener(controller);
+        getInventoryPanel().getBackButton().addActionListener(controller);
+        getCreateOrderPanel().getBackButton().addActionListener(controller);
+        getCreateOrderPanel().getPlaceOrder().addActionListener(controller);
     }
 
     @Override
@@ -117,14 +63,41 @@ public class InvView extends JFrame implements Observer {
         InvModel model = (InvModel) arg;
 
         if (!model.user.loginFlag) {
-            this.usernameInput.setText("");
-            this.passwordInput.setText("");
-            this.message.setText("Invalid username or password.");
+            getLoginPanel().setUsernameInput("");
+            getLoginPanel().setPasswordInput("");
+            getLoginPanel().setMessage("Invalid username or password.");
         } else if (model.user.loginFlag) {
             if (!model.user.isAdmin) {
                 model.getClient().setOrderList(model.clientOrders());
-                this.Viewer();
             }
         }
+    }
+
+    /**
+     * @return the loginPanel
+     */
+    public LoginPanel getLoginPanel() {
+        return loginPanel;
+    }
+
+    /**
+     * @return the clientMenuPanel
+     */
+    public ClientMenuPanel getClientMenuPanel() {
+        return clientMenuPanel;
+    }
+
+    /**
+     * @return the inventoryPanel
+     */
+    public InventoryPanel getInventoryPanel() {
+        return inventoryPanel;
+    }
+
+    /**
+     * @return the createOrderPanel
+     */
+    public CreateOrderPanel getCreateOrderPanel() {
+        return createOrderPanel;
     }
 }
